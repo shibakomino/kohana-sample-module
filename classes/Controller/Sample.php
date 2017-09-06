f0899862139a7d51ce2fe48de318282bcb075ca8
Colin Leung <colin@digi3studio.com>
2017-09-06 17:33:58 +0800

    sample module

---
+++ classes/Controller/Sample.php
@@ -0,0 +1,7 @@
+<?php
+
+Class Controller_Sample extends Controller{
+  public function action_index(){
+    echo 'Sample Controller is Running <br/>'.PHP_EOL;
+  }
+}
---
+++ init.php
@@ -0,0 +1,3 @@
+<?php
+
+echo 'Hello, the sample/init.php is running <br/>'.PHP_EOL;
