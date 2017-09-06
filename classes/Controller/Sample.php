e3fc034e4a622a7c133cd05679776492985fca07
Colin Leung <colin@digi3studio.com>
2017-09-07 02:16:00 +0800

    display content by using response body

--- classes/Controller/Sample.php
+++ classes/Controller/Sample.php
@@ -2,6 +2,6 @@
 
 Class Controller_Sample extends Controller{
   public function action_index(){
-    echo 'Sample Controller is Running <br/>'.PHP_EOL;
+      $this->response->body('Sample Controller is Running <br/>'.PHP_EOL);
   }
 }
